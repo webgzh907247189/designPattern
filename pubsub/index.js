43b064a4b88941154a3e1567980b619a4538c160
webgzh907247189 <18255106634@163.com>
2018-01-16 12:35:16 +0800

    Initial commit

---
+++ pubsub/index.js
@@ -0,0 +1,43 @@
+let obj = {
+	list: [],
+
+	// 订阅
+	listener: function (key,fn) {
+		// let this_list = this.list[key] 
+		// this_list = this_list || []
+
+		// this_list = [...this_list,fn]
+		// this_list.push(fn)
+
+
+
+		this.list[key] = this.list[key] || []
+
+		// this.list[key].push(fn)
+		this.list[key] = [...this.list[key],fn]
+	},
+
+	//发布
+	trigger: function(){
+		let key = Array.prototype.shift.apply(arguments)
+
+		let listFns = this.list[key]
+		if(listFns.length == 0 || !listFns){
+			return
+		}
+
+		for(let itemFn of listFns){
+			itemFn.apply(this,arguments)
+		}
+	}
+}
+
+obj.listener('eat',(food)=>{
+	console.log(`晚上吃的是${food}`)
+})
+obj.listener('drink',(water)=>{
+	console.log(`晚上喝的是${water}`)
+})
+
+obj.trigger('eat','面条')
+obj.trigger('drink','椰子水')
