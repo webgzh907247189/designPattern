d87c16262b8ac445c3e8018b53129a8f2bc170e8
gezhonghao <gezhonghao@hujiang.com>
2018-02-28 20:52:33 +0800

    1
    1

---
+++ commandMode/index.js
@@ -0,0 +1,121 @@
+{
+	let setCommand = function(button,func) {
+	    button.onclick = function(){
+	        func();
+	    }
+	};
+
+	let MenuBar = {
+	    refersh: function(){
+	        alert("刷新菜单界面");
+	    }
+	};
+
+	let SubMenu = {
+	    add: function(){
+	        alert("增加菜单");
+	    }
+	};
+
+
+	let RefreshMenuBarCommand = function(receiver) {
+	    return function(){
+	        receiver.refersh();    
+	    };
+	};
+
+	let AddSubMenuCommand = function(receiver) {
+	    return function(){
+	        receiver.add();    
+	    };
+	};
+
+	let refreshMenuBar =  RefreshMenuBarCommand(MenuBar)
+	let addSubMenu =  AddSubMenuCommand(SubMenu)
+
+	let b5 = document.getElementById("button5")
+    let b6 = document.getElementById("button6")
+	setCommand(b5,refreshMenuBar)
+	setCommand(b6,addSubMenu)
+}
+
+{
+	let b1 = document.getElementById("button1"),
+    b2 = document.getElementById("button2"),
+    b3 = document.getElementById("button3"),
+    b4 = document.getElementById("button4");
+
+    let bindEvent = function(btn,fn){
+    	btn.onclick = function(){
+    		fn()
+    	}
+    }
+
+    let Todo1 = {
+	    test1: function(){
+	        alert("我是来做第一个测试的");
+	    }    
+	};
+	let Menu = {
+	    add: function(){
+	        alert("我是来处理一些增加操作的");
+	    },
+	    del: function(){
+	        alert("我是来处理一些删除操作的");
+	    },
+	    update: function(){
+	        alert("我是来处理一些更新操作的");
+	    }
+	};
+
+	bindEvent(b1,Todo1.test1)
+	bindEvent(b2,Menu.add)
+	bindEvent(b3,Menu.del)
+	bindEvent(b4,Menu.update)
+}
+
+
+
+/**
+ * 理解宏命令
+ */
+{
+	let command1 = {
+	    execute: function(){
+	        console.log(1);
+	    }
+	};
+
+	let command2 = {
+	    execute: function(){
+	        console.log(2);
+	    }
+	};
+
+	let command3 = {
+	    execute: function(){
+	        console.log(3);
+	    }
+	};
+
+	function command(){
+		return {
+			objList: [],
+			add(command){
+				this.objList = [...this.objList,command]
+			},
+			execute(){
+				for(let item of this.objList){
+					item.execute()
+				}
+			}
+		}
+	}
+
+	let fnObj = command()
+	fnObj.add(command1)
+	fnObj.add(command2)
+	fnObj.add(command3)
+
+	fnObj.execute()
+}
--- dutyChainModel/index.js
+++ dutyChainModel/index.js
@@ -1,3 +1,12 @@
+/**
+ * 职责链模式的优点是：
+ * 		1. 解耦了请求发送者和N个接收者之间的复杂关系，不需要知道链中那个节点能处理你的请求，所以你只需要把请求传递到第一个节点即可。
+ *   	2. 链中的节点对象可以灵活地拆分重组，增加或删除一个节点，或者改变节点的位置都是很简单的事情。
+ *    	3. 我们还可以手动指定节点的起始位置，并不是说非得要从其实节点开始传递的.
+ *
+ * 缺点：职责链模式中多了一点节点对象，可能在某一次请求过程中，大部分节点没有起到实质性作用，他们的作用只是让
+ * 请求传递下去，从性能方面考虑，避免过长的职责链提高性能。
+ */
 {
 	function order500(orderType,isPay,count) {
 		if(orderType == 1 && isPay == true){
@@ -54,3 +63,58 @@
 	order500Chain.passRequest(3,true,500);  // 亲爱的用户，您已抽到10元优惠卷
 	order500Chain.passRequest(1,false,0);   // 亲爱的用户，请再接再厉哦
 }
+
+
+{
+	function Fn1() {
+	    console.log(1);
+	    return "nextSuccessor";
+	}
+
+	function Fn2() {
+	    console.log(2);
+	    let self = this;
+	    setTimeout(function(){
+	    	console.log(self) // this.successor -> chainFn2
+	        self.next();
+	    },1000);
+	}
+
+	function Fn3() {
+	    console.log(3);
+	}
+
+	class Chain{
+		constructor(fn){
+			this.fn = fn
+			this.successor = null
+		}
+
+		setNextSuccessor(successor){
+			return this.successor = successor
+		}
+
+		passRequest(){
+			let result = this.fn.apply(this,arguments)
+			if(result == 'nextSuccessor'){
+				this.successor.passRequest.apply(this.successor,arguments)
+			}
+		}
+
+		next(){
+			this.successor.passRequest.apply(this.successor,arguments)
+		}
+	}
+
+	let chainFn1 = new Chain(Fn1);
+	let chainFn2 = new Chain(Fn2);
+	let chainFn3 = new Chain(Fn3);
+
+	// 然后指定节点在职责链中的顺序
+	chainFn1.setNextSuccessor(chainFn2);
+	chainFn2.setNextSuccessor(chainFn3);
+
+	chainFn1.passRequest()    // 按照顺序打印1 2 3
+}
+
+//http://91porn.io/forum.php?x=52486
