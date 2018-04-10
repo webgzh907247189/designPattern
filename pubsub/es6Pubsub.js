a769bef0a1302568f54053a2c495985170d16f1e
gezhonghao <gezhonghao@hujiang.com>
2018-04-10 22:15:35 +0800

    1
    1

--- pubsub/es6Pubsub.js
+++ pubsub/es6Pubsub.js
@@ -1,3 +1,8 @@
+/**
+ * 使用ES6 实现发布订阅
+ * @type {Object}
+ * https://juejin.im/post/5a7d6ecc5188257a5911b9bb?utm_source=gold_browser_extension#heading-10
+ */
 let pubsub = {
 	listSet: new Set(),
 	listener(){
