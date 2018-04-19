222fc93ed96463aafd70c7b9693c0dc72198eb88
gezhonghao <gezhonghao@hujiang.com>
2018-04-19 21:54:19 +0800

    1
    1

---
+++ mediatorModel/test.js
@@ -0,0 +1,32 @@
+{
+	let obj = {name: '11',sex: '男',a: {b: 11}}
+	let o = {...obj,className: '一年级'}
+	obj.a.b = 22
+	console.log(o,'??',obj)
+	//{name: "11", sex: "男", a: {b: 22}, className: "一年级"}
+	//{name: "11", sex: "男", a: {b: 22}}
+}
+
+
+/** babel转换后的 */
+
+var _extends = Object.assign || function (target) { 
+	for (var i = 1; i < arguments.length; i++) { 
+		var source = arguments[i]; 
+		for (var key in source) { 
+			if (Object.prototype.hasOwnProperty.call(source, key)) { 
+				target[key] = source[key];
+			} 
+		} 
+	} 
+	return target; 
+};
+
+{
+	var obj = { name: '11', sex: '男', a: { b: 11 } };
+	var o = _extends({}, obj, { className: '一年级' });
+	obj.a.b = 22;
+	console.log(o, '??', obj);
+	//{name: "11", sex: "男", a: {b: 22}, className: "一年级"}
+	//{name: "11", sex: "男", a: {b: 22}}
+}
