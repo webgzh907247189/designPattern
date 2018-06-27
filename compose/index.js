23334cf971932d6c7a7a61458ca51c2a1a3be7c9
gezhonghao <gezhonghao@hujiang.com>
2018-06-27 21:11:46 +0800

    1
    1

---
+++ compose/index.js
@@ -0,0 +1,69 @@
+/**
+ * https://segmentfault.com/a/1190000008394749
+ */
+
+{
+	let compose = function (...args) {
+		let argslength = args.length;
+		let result
+
+		return function fn(...relayArgs){
+			result = args[argslength -1].apply(this,relayArgs)
+
+			if(argslength <= 1){
+				return result
+			}else{
+				argslength --
+				return fn.call(this,result)
+			}
+		}
+	}
+
+	let greeting = (firstName, lastName) => `hello, ${firstName} ${lastName}`;
+
+	let toUpper = str => str.toUpperCase();
+
+	let fn = compose(toUpper, greeting)
+	console.log(fn('  jack', 'smith   '))
+	// ‘HELLO，JACK SMITH’
+	
+
+	let trim = str => str.trim()
+	let fnTest = compose(trim, toUpper, greeting)
+	console.log(fnTest('jack', 'smith'))
+}
+
+
+/**
+ * lodash
+ */
+{
+	let flow = function(funs){
+		let fulength = funs.length
+		let fuIndex = fulength
+		while(fuIndex--){
+			if(typeof funs[fuIndex] != 'function'){
+				throw new Error('存在非法function')
+			}
+		}
+
+		return function(...args){
+			let index = 0
+			let result = fulength ? funs[index].apply(this,args) : funs[0]
+
+			while(++index < fulength){
+				result = funs[index].call(this,result)
+			}
+			return result
+		}
+	}
+
+	let greeting = (firstName, lastName) => `hello, ${firstName} ${lastName}`;
+
+	let toUpper = str => str.toUpperCase();
+
+	let trim = str => str.trim()
+
+	let fn = flow([greeting, toUpper, trim])
+	console.log(fn('  jack  ', 'smith  '))
+}
