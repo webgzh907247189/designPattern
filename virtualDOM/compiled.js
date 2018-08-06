ca36102ec238eb2a381954c644cc877ea1e34267
gezhonghao <gezhonghao@hujiang.com>
2018-08-06 15:36:30 +0800

    1
    1

--- virtualDOM/compiled.js
+++ virtualDOM/compiled.js
@@ -6,57 +6,50 @@
  */
 
 function view() {
-	return h("ul",
-	    { id: "filmList", className: "list" },
-	    h("li",{ className: "main" },"Detective Chinatown Vol 2"),
-	    h("li",null,"Ferdinand"),
-	    h("li",null,"Paddington 2")
-	);
+  return h("ul", { id: "filmList", className: "list" }, h("li", { className: "main" }, "Detective Chinatown Vol 2"), h("li", null, "Ferdinand"), h("li", null, "Paddington 2"));
 }
 
 function flatten(arr) {
-  	return [].concat(...arr)
+  return [].concat(...arr);
 }
 
 function h(type, props, ...children) {
-  	return {
-    	type,
-    	props: props || {},
-    	children: flatten(children)
-  	}
+  return {
+    type,
+    props: props || {},
+    children: flatten(children)
+  };
 }
 
 function render(el) {
-  	el.appendChild(createElement(view(0)))
+  el.appendChild(createElement(view(0)));
 }
 
-
 function createElement(node) {
-  	if (typeof(node) === 'string') {
-		return document.createTextNode(node)
-  	}
+  if (typeof node === 'string') {
+    return document.createTextNode(node);
+  }
 
-  	let { type, props, children } = node
-  	const el = document.createElement(type)
-  	setProps(el, props)
+  let { type, props, children } = node;
+  const el = document.createElement(type);
+  setProps(el, props);
 
-  	children.map(createElement).forEach(el.appendChild.bind(el))
-  	return el
+  children.map(createElement).forEach(el.appendChild.bind(el));
+  return el;
 }
 
 function setProp(target, name, value) {
-  	if (name === 'className') {
-    	return target.setAttribute('class', value)
-  	}
-  	target.setAttribute(name, value)
+  if (name === 'className') {
+    return target.setAttribute('class', value);
+  }
+  target.setAttribute(name, value);
 }
 
 function setProps(target, props) {
-  	Object.keys(props).forEach(key => {
-    	setProp(target, key, props[key])
-  	})
+  Object.keys(props).forEach(key => {
+    setProp(target, key, props[key]);
+  });
 }
-
 
 /**
  * babel-cli和babel-plugin-transform-react-jsx这两个库，前者提供Babel的命令行功能，后者主要帮我们把jsx转化成js。
