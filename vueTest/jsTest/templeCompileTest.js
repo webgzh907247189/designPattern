41d2141544472409f84d6a9aa7fdef0ea58730ce
gezhonghao <gezhonghao@hujiang.com>
2018-09-04 20:22:16 +0800

    1
    1

---
+++ vueTest/jsTest/templeCompileTest.js
@@ -0,0 +1,57 @@
+const ncname = '[a-zA-Z_][\\w\\-\\.]*';
+const qnameCapture = '((?:' + ncname + '\\:)?' + ncname + ')'
+const startTagOpen = new RegExp('^<' + qnameCapture)
+const singleAttrIdentifier = /([^\s"'<>/=]+)/
+const startTagClose = /^\s*(\/?)>/
+const singleAttrAssign = /(?:=)/
+const singleAttrValues = [
+  /"([^"]*)"+/.source,
+  /'([^']*)'+/.source,
+  /([^\s"'=<>`]+)/.source
+]
+
+const attribute = new RegExp(
+  '^\\s*' + singleAttrIdentifier.source +
+  '(?:\\s*(' + singleAttrAssign.source + ')' +
+  '\\s*(?:' + singleAttrValues.join('|') + '))?'
+)
+
+
+
+var html = '<div :class="c" class="demo" v-if="isShow"><span v-for="item in sz">{{item}}</span></div>';
+let index = 0;
+
+function advance (n) {
+    index += n
+    html = html.substring(n)
+}
+
+function parseStartTag () {
+    const start = html.match(startTagOpen);
+    if (start) {
+        const match = {
+            tagName: start[1],
+            attrs: [],
+            start: index
+        }
+        advance(start[0].length);
+
+        let end, attr
+        while (!(end = html.match(startTagClose)) && (attr = html.match(attribute))) {
+            advance(attr[0].length)
+            match.attrs.push({
+                name: attr[1],
+                value: attr[3]
+            });
+        }
+        if (end) {
+            match.unarySlash = end[1];
+            advance(end[0].length);
+            match.end = index;
+            return match
+        }
+    }
+}
+
+
+parseStartTag()
