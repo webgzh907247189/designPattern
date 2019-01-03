920a007364568332c7ba1b1c0450308412fdeea7
webgzh907247189 <18255106634@163.com>
2019-01-03 15:17:26 +0800

    update tag pakeage
    update tag pakeage

--- addTag/index.js
+++ addTag/index.js
@@ -10,20 +10,24 @@ function composeFn(...args){
 	}
 }
 
-function createTag([tagVersion,tagDes]){
-	console.log(tagVersion,'?????????????',tagDes,'111111111')
-	// let {code} = shell.exec(`git tag -a ${tagVersion} -m ${tagDes}`);
-	
-	let {code} = shell.exec(`git tag -a tagVersion -m tagDes`);
 
-	console.log(code,'222')
+function createTag([tagVersion,tagDes]){
+	let {code} = shell.exec(`git tag -a ${tagVersion} -m "${tagDes}"`);
 	return code
 }
 
-function pushTag(){
-	shell.exec('git push origin --tags')
+function pushTag(inpiutCode){
+	let {code} = !inpiutCode && shell.exec('git push origin --tags')
+	return {code,inpiutCode}
+}
+
+function outputConsole({code,inpiutCode}){
+	shell.echo({
+		'0_0': '\033[42;34m tag创建成功 && 成功推送 \033[0m',
+		'undefined_128': '\033[46;30m 重复创建 \033[0m'
+	}[`${code}_${inpiutCode}`] || '\033[41;30m 创建失败 \033[0m' )
 }
 
 
-let composeResult = composeFn(pushTag,createTag)
+let composeResult = composeFn(outputConsole,pushTag,createTag)
 composeResult(remain)
