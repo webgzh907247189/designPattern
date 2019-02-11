11cb9e2c02576b999b1c1f239562c615ec2ee517
webgzh907247189 <18255106634@163.com>
2019-02-11 11:46:08 +0800

    update  arrayDemo
    更新 数组demo

--- arrayDemo/index.js
+++ arrayDemo/index.js
@@ -3,9 +3,7 @@
  */
 var arr = ['codeZh', 'codeCn', 'taobao.cn', 'taobao.com'];
 
-
- {
-
+{
     let obj = arr.reduce((result,item)=>{
         if(item.includes('code')){
             getObj(result,item,'code')
@@ -21,13 +19,52 @@ var arr = ['codeZh', 'codeCn', 'taobao.cn', 'taobao.com'];
     
     function getObj(result,item,key){
         let [code,lan] = stringsplit(item,key)
-        result[code] = result[code] ||{}
+        result[code] = result[code] || {}
         result[code][lan] = item
         return result
     }
     console.log(obj)
- }
+}
+
+/**
+ * {
+        "code": {
+            "Zh": "codeZh",
+            "Cn": "codeCn"
+        },
+        "taobao": {
+            ".cn": "taobao.cn",
+            ".com": "taobao.com"
+        }
+    }
+*/
 
 
 
- 
+
+
+
+
+
+{
+    const list = ['codeZh', 'codeCn', 'taobao.cn', 'taobao.com']
+    const result = list.reduce((map, item) => {
+        const [head, tail] = item.split(/(?=[A-Z]|\.)/)  // ? 匹配表达式零次或一次
+        map[head] = map[head] || {}
+        map[head][tail] = item
+        return map
+    }, {})
+    console.log(result)
+}
+
+
+
+{
+    const result = {} 
+    const list = ['codeZh', 'codeCn', 'taobao.cn', 'taobao.com'] 
+    list.join().replace(/([a-z]+)([A-Z\.][a-z]+)/g, (m, c0, c1) => { 
+        result[c0] = result[c0] || {} 
+        result[c0][c1] = m 
+    }) 
+    console.log(result)
+}
