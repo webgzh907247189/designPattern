551cfac5a7e69346b9ac0b0fd4bbcc3031f65b99
webgzh907247189 <18255106634@163.com>
2019-02-11 16:50:57 +0800

    update 修正只是盲点， Object.getProtoOf() && Object.setProtoOf()

--- vuePrinciple/vueObserver.js
+++ vuePrinciple/vueObserver.js
@@ -51,6 +51,10 @@
 }
 
 
+
+
+
+
 {
 	let obj = {
 		eat(){
@@ -67,6 +71,39 @@
 
 	console.log(o.eat())  // 吃饭 | 喝水
 }
+
+{
+	let obj = {
+		eat(){
+			return '吃饭'
+		}
+	}
+
+	let o = {
+		eat(){
+			return `${super.eat()} | 喝水`
+		}
+	}
+
+	Object.setPrototypeOf(o,obj)
+	console.log(o.eat())  // 吃饭 | 喝水
+}
+
+/**
+ * 如果第一个参数不是对象，会自动转为对象。但是由于返回的还是第一个参数，所以这个操作不会产生任何效果。
+ */
+{
+	Object.setPrototypeOf(1, {}) === 1 // true
+	Object.setPrototypeOf('foo', {}) === 'foo' // true
+	Object.setPrototypeOf(true, {}) === true // true
+}
+
+
+
+
+
+
+
 
 {
 	let fn = function (argument) {
@@ -129,8 +166,98 @@
 
 	console.log(person1,person2,person3)
 	/**
-	 * person2 丢失了 setter ，因为它没有复制过来。
+	 * person2 丢失了 setter ，因为它没有复制过来。  赋值之后，只有person2有 name 值
 	 *
 	 * 使用 Object.create() 对浅拷贝对象也有同样的限制。
 	 */
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Object.fromEntries()方法是Object.entries()的逆操作，用于将一个键值对数组转为对象。
+ */
+{
+	let obj = Object.fromEntries([ ['foo', 'bar'],['baz', 42] ])
+	// { foo: "bar", baz: 42 }
+}
+
+{
+	const entries = new Map([
+		['foo', 'bar'],
+		['baz', 42]
+	]);
+	  
+	let obj = Object.fromEntries(entries)
+	// { foo: "bar", baz: 42 }
+}
+
+{
+	const map = new Map().set('foo', true).set('bar', false);
+	let obj = Object.fromEntries(map)
+	// { foo: true, bar: false }
+
+	let queryObj = Object.fromEntries(new URLSearchParams('foo=bar&baz=qux'))
+	// { foo: "bar", baz: "qux" }
+}
+
+
+
+
+
+
+
+
+
+
+{
+	let paramsString = "q=URLUtils.searchParams&topic=api"
+	let searchParams = new URLSearchParams(paramsString);
+
+	for (let p of searchParams) {
+		console.log(p); // ["q", "URLUtils.searchParams"]   ["topic", "api"]
+	}
+
+	for (let p of searchParams.entries()) {
+		console.log(p); // ["q", "URLUtils.searchParams"]   ["topic", "api"]
+	}
+
+	searchParams.has("topic") === true; // true
+
+	searchParams.get("topic") === "api"; // true
+
+	searchParams.getAll("topic"); // ["api"]
+
+	searchParams.get("foo") === ""; // false   searchParams.get("foo") === null
+
+	searchParams.append("topic", "webdev");
+	searchParams.toString(); // "q=URLUtils.searchParams&topic=api&topic=webdev"
+
+	searchParams.set("topic", "More webdev");   // 设置一个搜索参数的新值，假如原来有多个值将删除其他所有的值 (覆盖原有的值)。
+	searchParams.toString(); // "q=URLUtils.searchParams&topic=More+webdev"
+
+	searchParams.delete("topic");
+	searchParams.toString(); // "q=URLUtils.searchParams"
+
+	for (let p of searchParams.keys()) {
+		console.log(p); // q
+
+	for (let p of searchParams.values()) {
+		console.log(p); // URLUtils.searchParams
+	}
 }
