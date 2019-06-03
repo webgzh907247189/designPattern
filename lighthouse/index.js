2955d9fdd37d0f90edb8f371d474b2179f98d11c
webgzh907247189 <18255106634@163.com>
2019-06-03 23:16:42 +0800

    lighthouse 生成站点报告

---
+++ lighthouse/index.js
@@ -0,0 +1,10 @@
+/**
+ * 生成站点报告
+ * 
+ * https://juejin.im/book/5b936540f265da0a9624b04b/section/5bb6218ee51d450e7762f873
+ * 
+ * 掘金小册 (前端性能优化原理与实践)
+ */
+
+npm install -g lighthouse
+lighthouse https://juejin.im/books --view
