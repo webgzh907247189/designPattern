3f7dce7236410f4e73887e3210206e4576e142f3
webgzh907247189 <18255106634@163.com>
2019-06-05 11:16:19 +0800

    前端优化

--- lighthouse/index.js
+++ lighthouse/index.js
@@ -10,7 +10,17 @@
 // lighthouse https://juejin.im/books --view
 
 
-let fs = require('fs')
-fs.readdir('./',function(err,files){
-    console.log(files)
-})
+
+{
+    
+0. 前端优化 (网络优化,渲染优化)
+1. 深入了解http缓存http请求相关知识
+2. 了解浏览器渲染原理
+
+(解析 HTML dom -> 计算样式 cssom  -> 计算图层布局 render tree -> 绘制图层 painer  ->  整合图层，得到页面 将数据由 CPU 输出给 GPU 最终绘制在屏幕上)
+复杂的视图层会给这个阶段的 GPU 计算带来一些压力，在实际应用中为了优化动画性能，我们有时会手动区分不同的图层
+
+当前可视区域的高度 ->  window.innerHeight || document.documentElement.clientHeight 
+元素距离可视区域顶部的高度 -> getBoundingClientRect()
+
+}
