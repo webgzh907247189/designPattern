80824daa8997aff079209fafdc9b0668e54c3a43
webgzh907247189 <18255106634@163.com>
2019-07-25 14:26:39 +0800

    源码的 combineReducers

--- myreacttest/src/my/redux/combineReducers.js
+++ myreacttest/src/my/redux/combineReducers.js
@@ -9,3 +9,28 @@ function combineReducers(reducers){
 }
 
 export default combineReducers
+
+// 源码的 combineReducers
+function combineReducers1(reducers){
+    let reducersKeys = Object.keys(reducers)
+    return (state,action) => {
+        // 此次派发动作 是否引起状态改变
+        let hasChanged = false 
+
+        let stateObj = Object.create(null)
+        for(let i=0; i<reducersKeys.length; i++){
+
+            let key = reducersKeys[i]
+            let reducer = reducers[key]
+            let prevReducerState = state[key]
+
+            let nextReducerState = reducer(prevReducerState,action)
+            stateObj[key] = nextReducerState
+
+            // 在循环里面，有一次 hasChanged 为true，后面的判断就不走了。减少执行。所以加了 hasChanged || prevReducerState !== nextReducerState
+            // hasChanged = prevReducerState !== nextReducerState
+            hasChanged = hasChanged || prevReducerState !== nextReducerState
+        }
+        return hasChanged ? stateObj : state
+    }
+}
