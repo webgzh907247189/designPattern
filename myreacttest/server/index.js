b3f79adbb50ef6f803279bb7457fe7da4562d509
webgzh907247189 <18255106634@163.com>
2019-08-13 15:50:10 +0800

    ssr

--- myreacttest/server/index.js
+++ myreacttest/server/index.js
@@ -1,6 +1,7 @@
 let express = require('express')
 
-// import React from 'react'
+// Vdom 需要react，因为是依赖 React.createElement()
+import React from 'react' 
 import MyCtx from '../src/myContext'
 import {renderToString} from 'react-dom/server'
 
@@ -30,4 +31,12 @@ app.listen(3000,function(){
  * babel-loader 转译   具体的转换操作(babel-core 编译的环境)，真正的转换靠 babel-preset-env(预设 es6+ 转成es5)
  * 
  * ssr中没有 window，没有dom，但是存在 fs这些node原生模块
+ * 
+ * ssr 
+ * 服务器端运行代码产生html
+ * 服务端把html发送给浏览器
+ * 浏览器收到html，渲染
+ * 浏览器引入js下载  还是需要依赖js 绑定事件，绑定事件只有在浏览器进行
+ * 执行已经下载的js
+ * 浏览器的代码接管页面，后面与浏览器渲染是一样的
  */
