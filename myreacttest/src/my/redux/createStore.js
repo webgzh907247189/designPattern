95b3c1eab104ceb3ca3abd29877a5424ccc656a1
webgzh907247189 <18255106634@163.com>
2019-08-22 17:18:47 +0800

    redux 新增辅助函数判断

--- myreacttest/src/my/redux/createStore.js
+++ myreacttest/src/my/redux/createStore.js
@@ -1,3 +1,44 @@
+// 这个函数用于判断当前代码是否已经被打包工具（比如 Webpack）压缩过，如果被压缩过的话，
+// isCrushed 函数的名称会被替换掉。如果被替换了函数名但是 process.env.NODE_ENV 又不等于 production
+// 的时候，提醒用户使用生产环境下的精简代码。
+function isCrushed() {}
+    if ( process.env.NODE_ENV !== 'production' && typeof isCrushed.name === 'string' && isCrushed.name !== 'isCrushed') {
+        warning(
+        'You are currently using minified code outside of NODE_ENV === "production". ' +
+        'This means that you are running a slower development build of Redux. ' +
+        'You can use loose-envify (https://github.com/zertosh/loose-envify) for browserify ' +
+        'or setting mode to production in webpack (https://webpack.js.org/concepts/mode/) ' +
+        'to ensure you have the correct code for your production build.'
+    )
+}
+
+
+// 判断传入的对象是否是纯对象，因为 redux 要求 action 和 state 是一个纯对象，所以这个函数诞生了。
+// 正常情况下 proto === Object.prototype(因为Object.getPrototypeOf(Object.prototype) === null)，倒数第二个层级 null -> Object.prototype
+function isPlainObject(obj) {
+    if (typeof obj !== 'object' || obj === null) return false
+  
+    let proto = obj
+    // 获取最顶级的原型，如果就是自身，那么说明是纯对象。
+    while (Object.getPrototypeOf(proto) !== null) {
+      proto = Object.getPrototypeOf(proto)
+    }
+  
+    return Object.getPrototypeOf(obj) === proto
+}
+
+
+function warning(message) {
+    if (typeof console !== 'undefined' && typeof console.error === 'function') {
+      console.error(message)
+    }
+    try {
+      throw new Error(message)
+    } catch (e) {} 
+}
+  
+
+
 function createStore(reducer,initState){
     let state = initState
     let listeners = []
@@ -11,6 +52,9 @@ function createStore(reducer,initState){
     }
 
     // 调用dispatch 返回一个新的状态
+    // subscribe 方法就是对观察者模式的利用（注意不是发布订阅模式，二者有区别，不要混淆），我们通过 subscribe 方法注册我们的函数，
+    // 我们的函数会给存储到 createStore 方法的一个局部变量当中，每次 dispatch 被调用之后，都会遍历一遍 currentListeners，
+    // 依次执行其中的方法，达到我们订阅的要求。
     function dispatch(action){
         state = reducer(state,action)
         listeners.forEach((item)=>{
