2eecdd220b300b773425efc2559b9271c24da940
gezhonghao <gezhonghao@innotechx.com>
2019-10-30 14:49:19 +0800

    新增node单元测试 && 覆盖率

---
+++ node/node_test/index.js
@@ -0,0 +1,29 @@
+function fibonacci(n) {
+    if(n === 0){
+        return 0
+    }
+
+    if(n === 1){
+        return 1
+    }
+
+    if(n > 10){
+        return '暂时不支持计算'
+    }
+
+    if(n < 0){
+        return '暂时不支持计算'
+    }
+
+    return fibonacci(n-1) + fibonacci(n-2)
+}
+
+exports.fibonacci = fibonacci
+
+
+if (require.main === module) {
+    // 如果是直接执行 main.js，则进入此处
+    // 如果 main.js 被其他文件 require，则此处不会执行。
+    let n = Number(process.argv[2]);
+    console.log('fibonacci(' + n + ') is', fibonacci(n));
+}
