7bd3c6ef60b63ab8b154e709dfa5284c7be93495
webgzh907247189 <18255106634@163.com>
2020-03-22 11:07:44 +0800

    webworker

--- upload/upload1/work.js
+++ upload/upload1/work.js
@@ -1,6 +1,10 @@
 self.importScripts("/md5.min.js"); // 导入脚本
 self.importScripts("/ajax.js"); 
 
+// 在worker里进行的复杂计算，运行时间并不会变短，有时耗费时间甚至更长，毕竟开启worker也需要消耗一定的性能
+// 这样主线程就能及时响应用户操作而不会造成卡顿现象
+
+
 // 生成文件 hash
 self.onmessage = e => {
   const { name } = e.data;
