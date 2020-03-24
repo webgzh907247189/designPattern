8571814738fa8e5fc33ca6d1f37722a946ad7114
gezhonghao <gezhonghao@innotechx.com>
2020-03-24 14:44:54 +0800

    abortController

--- upload/upload1/abortCOntroller.js
+++ upload/upload1/abortCOntroller.js
@@ -1,53 +1,41 @@
 {
     let abortController = null; // 2
 
-    document.querySelector('#calculate').addEventListener('click', async ({ target }) => {
+    async function test(){
         if (abortController) {
             abortController.abort(); // 5
 
             abortController = null;
-            target.innerText = 'Calculate';
-
             return;
-
         }
 
         abortController = new AbortController(); // 3
-        target.innerText = 'Stop calculation';
-
         try {
             const result = await calculate(abortController.signal); // 4
-
             alert(result);
-
         } catch {
             alert('WHY DID YOU DO THAT?!'); // 9
-
-        } finally { // 10
-            abortController = null;
-            target.innerText = 'Calculate';
-
         }
-
-    });
+    };
 
     function calculate(abortSignal) {
         return new Promise((resolve, reject) => {
+
             const timeout = setTimeout(() => {
                 resolve(1);
-
             }, 5000);
 
             abortSignal.addEventListener('abort', () => { // 6
                 const error = new DOMException('Calculation aborted by the user', 'AbortError');
 
                 clearTimeout(timeout); // 7
+                console.log(error)
                 reject(error); // 8
-
             });
 
         });
 
     }
 
+    test();
 }
