0ab9e199e59e3cdce9745521084bbfb97b0725d7
webgzh907247189 <18255106634@163.com>
2020-04-23 02:33:21 +0800

    vue

--- new_Vue/index.js
+++ new_Vue/index.js
@@ -19,11 +19,48 @@ Vue.prototype._init = function(options){
     }
 }
 
+// ?: 匹配不捕获
+// var s = /\{\{([^}]+)\}\}/g
+const defaultRe = /\{\{((?:.|\r?\n)+?)\}\}/g;
+const util = {
+    getValue(vm,expr){
+        return expr.split('.').reduce((memo,current) => {
+            memo = memo[current]
+            return memo;
+        },vm)
+    },
+    compileText(node,vm){
+        node.textContent = node.textContent.replace(defaultRe,function(...args){
+            return util.getValue(vm,args[1])
+        }) 
+    }
+}
+function compile(node,vm){
+    let childNodes = node.childNodes
+
+    Array.from(childNodes).forEach(child => {
+        if(child.nodeType === 1){ // 1 -> 元素   3 -> 文本
+            compile(child,vm) // 递归边缘
+        }else if(child.nodeType === 3){
+            util.compileText(child,vm)
+        }
+    });
+}
+
 Vue.prototype._update = function(){
     // 用户传入的数据去更新试图
     let vm = this;
     let el = vm.$el;
 
+    let node = document.createDocumentFragment()
+
+    let firstChild
+    while (firstChild = el.firstChild) {
+        node.appendChild(firstChild)
+    }
+
+    compile(node,vm)
+    el.appendChild(node)
     // 需要使用 {{}} 的方式进行替换
     
 }
