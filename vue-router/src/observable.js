d551d68b837199621d6dc6ccae9c05e08b65da9c
gezhonghao <gezhonghao@innotechx.com>
2020-05-20 14:39:02 +0800

    从后向前进行代理

--- vue-router/src/observable.js
+++ vue-router/src/observable.js
@@ -24,3 +24,52 @@ let obj = {name: '111',sex: {title: 'nan'}}
 
 let proxyObj = observable(obj,handle)
 console.log(proxyObj)
+
+
+
+
+{
+    const getTypeFn = (type) => {
+        return (target) => {
+            return Object.prototype.toString.call(target) === `[object ${type}]`;
+        };
+    };
+    const isObject = getTypeFn('Object');
+
+    function observable(obj,handle){
+        if(typeof obj !== 'object') return obj
+        
+        // 从后往前实现代理
+        Object.keys(obj).forEach((item)=>{
+            obj[item] = observable(obj[item],handle)
+        })
+        return new Proxy(obj,handle())
+    }
+
+    function handle(){
+        return {
+            get(target,key,value){
+                const result = Reflect.get(target,key) || '暂无此值'
+                return result;
+            },
+            set(){
+                return Reflect.set(target,key,value)
+            }
+        }
+    }
+
+    const obj = {name: '111',sex: {
+        home: '',
+        age: 'zzz',
+        eat: {
+            price: '1',
+            price1: ''
+        }
+    }}
+
+    let proxyObj = observable(obj,handle)
+    console.log(proxyObj)
+
+    // obj.sex.eat.price1 ->  "暂无此值"
+    // obj.sex.eat.price  ->  1
+}
