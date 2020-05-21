eedd20be10cb67d86a9ebe0d3764ee8e2fb11918
gezhonghao <gezhonghao@innotechx.com>
2020-05-21 21:09:56 +0800

    Fibonacci 优化

--- compose/index.js
+++ compose/index.js
@@ -412,6 +412,31 @@
 }
 
 
+{
+	// 尾递归优化过的 Fibonacci 数列实现如下
+	function Fibonacci2 (n , ac1 = 1 , ac2 = 1) {
+		if( n <= 1 ) {return ac2};
+	
+		return Fibonacci2 (n - 1, ac2, ac1 + ac2);
+	}
+	
+	// Fibonacci2(100) // 573147844013817200000
+	// Fibonacci2(1000) // 7.0330367711422765e+208
+
+
+	// 非尾递归的 Fibonacci  容易爆栈
+	function Fibonacci (n) {
+		if ( n <= 1 ) {return 1};
+	
+		return Fibonacci(n - 1) + Fibonacci(n - 2);
+	}
+	
+	// Fibonacci(10) // 89
+	// Fibonacci(100) // 超时
+	// Fibonacci(500) // 超时
+}
+
+
 
 {
 	// function compose(...funcs) {
