97bae6d6badf7599484a873ac2e3e6abb75f72e9
gezhonghao <gezhonghao@innotechx.com>
2020-05-30 20:16:42 +0800

    更正发布订阅知识点

--- pubsub/index.js
+++ pubsub/index.js
@@ -458,3 +458,56 @@
  * 
  * https://juejin.im/post/5c55512af265da2deb6a7dc8  (NodeJS Events模块源码学习)
  */
+
+
+ {
+	class EventEmitter {
+		constructor() {
+		  this.events = {};
+		}
+
+		// 实现订阅
+		on(type, callBack) {
+		  if (!this.events) this.events = Object.create(null);
+	  
+		  if (!this.events[type]) {
+			this.events[type] = [callBack];
+		  } else {
+			this.events[type].push(callBack);
+		  }
+		}
+
+		// 删除订阅
+		off(type, callBack) {
+		  if (!this.events[type]) return;
+		  this.events[type] = this.events[type].filter(item => {
+			return item !== callBack;
+		  });
+		}
+
+		// 只执行一次订阅事件
+		once(type, callBack) {
+		  function fn() {
+			callBack();
+			this.off(type, fn);
+		  }
+		  this.on(type, fn);
+		}
+
+		// 触发事件
+		emit(type, ...rest) {
+		  this.events[type] && this.events[type].forEach(fn => fn.apply(this, rest));
+		}
+	  }
+	  // 使用如下
+	  const event = new EventEmitter();
+
+	  function ss(){
+		  console.log('???')
+	  }
+	  
+	  event.once("dbClick", ss);
+	  event.once("dbClick", ss);
+
+	  event.emit('dbClick')
+ }
