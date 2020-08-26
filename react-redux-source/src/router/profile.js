48b12dd8716feb36441e4b3882cc4b0f1471952b
gezhonghao <gezhonghao@innotechx.com>
2020-08-26 22:50:33 +0800

    1

--- react-redux-source/src/router/profile.js
+++ react-redux-source/src/router/profile.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
-export default class Profile extends React.Component{
+class Profile extends React.Component{
+    onProfile = () => {
+        this.props.settle('1111')
+    }
+
     render(){
         return <>
-            Profile
+            <button onClick={this.onProfile}>个人中心Profile</button>
         </>
     }
 }
+
+function widthProfile(Com){
+    return class extends React.Component{
+        settle = (...args) => {
+            console.log(...args, '高阶组件的传递函数下去，子组件帮忙执行')
+        }
+
+        render(){
+            return <Com settle={this.settle}/>
+        }
+    }
+}
+
+export default widthProfile(Profile)
