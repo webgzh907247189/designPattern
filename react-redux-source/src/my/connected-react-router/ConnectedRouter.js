623018c8bf97456338392e32e18adda5434e19fa
gezhonghao <gezhonghao@innotechx.com>
2020-08-31 23:19:52 +0800

    connected-react-router

--- react-redux-source/src/my/connected-react-router/ConnectedRouter.js
+++ react-redux-source/src/my/connected-react-router/ConnectedRouter.js
@@ -1,11 +1,23 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import { LOCATION_CHANGE } from "./action";
+import { ReactReduxContext } from "react-redux";
 
 // 当派发一个动作，告诉仓库修改状态
 export default class ConnectedRouter extends React.Component{
+    static contextType = ReactReduxContext
+    
     componentDidMount(){
+        this.unListener = this.props.history.listen((location, action) => {
+            this.context.store.dispatch({
+                type: LOCATION_CHANGE,
+                payload: {location,action}
+            })
+        })
+    }
 
+    componentWillUnmount(){
+        this.unListener();
     }
 
     render(){
--- react-redux-source/src/my/connected-react-router/connectRouter.js
+++ react-redux-source/src/my/connected-react-router/connectRouter.js
@@ -1,6 +1,13 @@
+import { LOCATION_CHANGE } from "./action";
+
 // 是一个reducer
 export default function(history){
-    return (state,action) => {
+    let initState = {action: history.action, location: history.location}
+    return (state = initState,action) => {
+        if(action.type === LOCATION_CHANGE){
+            return action.payload
+        }
 
+        return state
     }
 }
