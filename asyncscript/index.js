961e844535eebfc0016d48d5eac4d270502b24bd
gezhonghao <gezhonghao@innotechx.com>
2020-10-22 14:43:13 +0800

    async & defer & type="module" 更新

--- asyncscript/index.js
+++ asyncscript/index.js
@@ -1,8 +1,15 @@
 /**
+ * https://mp.weixin.qq.com/s/0hmeE0JbNMF1Rx7_p8oAew (写的不错)
  * https://juejin.im/post/6881231745073561614
  * https://www.cnblogs.com/jiasm/p/7683930.html(未看完) 
  * 
- * 1. async 俗称异步执行脚本
+ * 
+ * <script> 的属性可以加上 type="module"。这时浏览器会认为这个文件是一个JavaScript 模块，其中的解析规则、执行环境会略有不同；
+ * 这时 <script> 的默认行为会像是 defer 一样，在后台下载，并且等待 DOM 解析、渲染完成之后才会执行，
+ * 所以 defer 属性无法在 type="module" 的情况下发生作用。但同样可以通过  async 属性使它在下载完成后即刻执行。
+ * 
+ * 
+ * 1. async 俗称异步执行脚本 (下载后会立刻执行，且不保证执行顺序)
  * 2. async 是告诉浏览器,可以不必等到它下载解析完后再加载页面,也不用等它执行完后再执行其他脚本,  
  * 3. 多个async无法保证他们的执行顺序,例如async1和async2无法按顺序执行
  * 4. 解析到script标签后,async是直接下载
