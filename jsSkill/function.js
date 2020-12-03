a1a2217f70e49e9d84c87901ae573be526747de2
gzh <18255106634@163.com>
2020-12-03 11:19:33 +0800

    debounce 调整

--- jsSkill/function.js
+++ jsSkill/function.js
@@ -3,26 +3,24 @@
  */
 // 防抖 (规定时间内回调函数只能执行一次，如果在规定时间内又触发了该事件，则会重新开始算规定时间)
 {
-    function debounce(fun, delay = 1000, immediate = true) {
+    function debounce(fun, delay = 1000, immediate = false) {
         let timer = null; //保存定时器
-        return function (args) {
+        return function (...args) {
             let that = this;
-            let _args = args;
-            if (timer)
-                clearTimeout(timer); //不管是否立即执行都需要首先清空定时器
+            if (timer) clearTimeout(timer); //不管是否立即执行都需要首先清空定时器
+
             if (immediate) {
                 if (!timer) {
-                    fun.call(that, _args);
+                    fun.apply(that, args);
                 } //如果定时器不存在,则说明延时已过,可以立即执行函数
                 //不管上一个延时是否完成,都需要重置定时器
                 timer = setTimeout(function () {
                     timer = null; //到时间后,定时器自动设为null,不仅方便判断定时器状态还能避免内存泄露
                 }, delay);
-            }
-            else {
+            } else {
                 //如果是非立即执行版,则重新设定定时器,并将回调函数放入其中
                 timer = setTimeout(function () {
-                    fun.call(that, _args);
+                    fun.apply(that, args);
                 }, delay);
             }
         };
