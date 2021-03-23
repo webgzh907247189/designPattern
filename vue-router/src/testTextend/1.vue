ad95fd9e98c36714db9095b18ba889e7ccb9ba6e
gzh <18255106634@163.com>
2021-03-23 16:05:14 +0800

    vue extends 系列

---
+++ vue-router/src/testTextend/1.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="aa">Children1</div>
+</template>
+
+<script>
+/**
+ * data：递归合并，如有冲突，组件数据优先；
+ * 钩子函数都会调用，组件自身钩子后执行；
+ * 值为对象的选项，只会进行第一层合并，如有冲突，组件数据优先
+ * css是根据选择器去合并和覆盖的；
+ */
+import dataExtends from './2';
+export default {
+    name: "Children1",
+    extends: dataExtends,
+    data() {
+        return {
+            aa: 'Children1 Render'
+        };
+    },
+    beforeCreate() {
+        console.log("==============Children1 beforeCreate start==================");
+        console.log(this.aa);
+    },
+    created() {
+        console.log("==============Children1 created start==================");
+    },
+    beforeMount() {
+        console.log("==============Children1 beforeMount start==================");
+    },
+    mounted() {
+        console.log("==============Children1 mounted start==================");
+    },
+    beforeUpdate() {
+        console.log("==============Children1 beforeUpdate start==================");
+    },
+    updated() {
+        console.log("==============Children1 updated start==================");
+    },
+    beforeDestroy() {
+        console.log("==============Children1 beforeDestroy start==================");
+    },
+    destroyed() {
+        console.log("==============Children1 destroyed start==================");
+    }
+}           
+</script>
---
+++ vue-router/src/testTextend/2.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        1223213
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            aa: 'Base'
+        }
+    },
+    render() {
+        return this.aa
+    },
+    beforeCreate() {
+        console.log("==============Base beforeCreate start==================");
+    },
+    created() {
+        console.log("==============Base created start==================");
+        console.log(this.aa);
+    },
+    beforeMount() {
+        console.log("==============Base beforeMount start==================");
+    },
+    mounted() {
+        console.log("==============Base mounted start==================");
+    },
+    beforeUpdate() {
+        console.log("==============Base beforeUpdate start==================");
+    },
+    updated() {
+        console.log("==============Base updated start==================");
+    },
+    beforeDestroy() {
+        console.log("==============Base beforeDestroy start==================");
+    },
+    destroyed() {
+        console.log("==============Base destroyed start==================");
+    }
+}
+</script>
