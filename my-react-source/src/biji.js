7e307906a4a4d0dc3b553b509cb5d7a91ba71460
gzh <18255106634@163.com>
2022-03-05 15:17:42 +0800

    区分 react 类组件和 函数组件

---
+++ my-react-source/src/biji.js
@@ -0,0 +1,6 @@
+// react 区分函数组件和类组件 使用 isReactComponent 来区分
+
+// function Component(){}
+// Component.prototype.isReactComponent = {}
+
+// https://github.com/facebook/react/blob/cb1e7b1c6ccd989d54b376ee4ae9da72a34f96e5/packages/react/src/ReactBaseClasses.js#L29
