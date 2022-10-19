a7cddc612b6c24980a000b53ff5ccdd7a87e4123
Zhonghao GE 葛钟昊 <zhonghao.ge.o@nio.com>
2022-10-19 14:30:41 +0800

    feat: vue的 watcher 理解

--- new_Vue/observe/dep.js
+++ new_Vue/observe/dep.js
@@ -5,6 +5,8 @@ let id = 0;
  * 
  * template 会被编译成render函数，函数执行的时候，访问什么变量，就出触发相应变量的get，然后才会添加watcher。
  * 触发了 get，然后在 get里面 (每一个data 属性都有一个 dep 实列), 触发了dep.depend() ---> watcher.addDep ---> dep.addSub(dep 把 wacther 加进来)。 dep里面有 wacther， wacter 里面有dep
+ * 
+ * 同一个的 Watcher 在同一个 tick 的时候应该只被执行一次，也就是说队列 queue 中不应该出现重复的 Watcher 对象。
  */
 class Dep{
     constructor(props) {
