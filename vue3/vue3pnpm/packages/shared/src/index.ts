35b6d55ffe0f592b06f60c03c8421e7ffed76bfd
jonas <18255106634@163.com>
2026-03-29 17:43:34 +0800

    feat: add comment

--- vue3/vue3pnpm/packages/shared/src/index.ts
+++ vue3/vue3pnpm/packages/shared/src/index.ts
@@ -18,10 +18,32 @@ export const isArray = Array.isArray
 
 export const assign = Object.assign;
 
+
+
+
+
+/**
+ * 位运算案例
+ */
+
+
+// 权限的组合可以使用 | 的方式
+/**
+ * 001 = 1  普通用户的权限
+ * 010 = 2  管理员的权限
+ * 100 = 4  超级管理员的权限
+ * 
+ * 
+ * 一个角色的权限运算出来是: 011    (由 001 | 010 运算得来)
+ * 
+ * 011 & 001 > 0  说明包含   普通用户的权限
+ * 011 & 010 > 0  说明包含   管理员的权限
+ * 011 & 100 < 0  说明不包含  超级管理员的权限
+ */
 export const enum ShapeFlags {
     ELEMENT = 1,
-    FUNCTIONAL_COMPONENT = 2, // 1 << 1
-    STATEFUL_COMPONENT = 4, // 1 << 2
+    FUNCTIONAL_COMPONENT = 2, // 1 << 1  (函数式组件)
+    STATEFUL_COMPONENT = 4, // 1 << 2    (普通状态组件)
     TEXT_CHILDREN = 8, // 1 << 3        // 儿子是文本
     ARRAY_CHILDREN = 16, // 1 << 4      // 儿子是数组 
     SLOTS_CHILDREN = 32, // 1 << 5
