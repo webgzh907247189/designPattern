40ecbf12e402a97551a58d7568de37e787157690
jonas <18255106634@163.com>
2026-03-29 15:06:02 +0800

    feat: add comment

--- vue3/vue3pnpm/packages/runtime-dom/src/index.ts
+++ vue3/vue3pnpm/packages/runtime-dom/src/index.ts
@@ -10,6 +10,9 @@ const renderOptions = Object.assign({}, nodeOps, { patchProps })
 
 // runtime-dom， 用户可以直接使用该方法进行调用，默认集合了 renderOptions
 // 用户也可以手动 调用 runtime-core 的 createRenderer，但是需要配置参数
+
+
+// createApp 底层调用了 createRenderer
 export const render = (vnode, container) => {
     console.log(renderOptions, 'renderOptions')
     return createRenderer(renderOptions).render(vnode, container)
