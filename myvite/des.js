0ad5ef1054f81387f3c29e2aaac7e0e9b1fccb86
jonas <18255106634@163.com>
2026-04-23 15:32:36 +0800

    feat: update

---
+++ myvite/des.js
@@ -0,0 +1,15 @@
+// pacakge.json 中带有 bin 字段的模块，执行 npm link， 自动安装到全局
+
+
+// 该项目中已链接的包
+// npm ls --depth=0
+
+
+
+// 列出全局模块目录中已链接的包
+// npm ls -g --depth=0
+
+
+
+// 解除全局 link
+// npm uninstall -g myvite
